e998aee7c82d6f13681fa41ce71e7d00c8f25e7d
tanu <ttanu5451@gmail.com>
2024-01-31 00:29:39 +0530

    Testing1

---
+++ src/test/java/Utils/TextContextSetup.java
@@ -0,0 +1,17 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+
+import Resource.BaseTest;
+
+
+public class TextContextSetup extends BaseTest{
+	//public WebDriver driver;
+	public String landingpageproductname;
+	public String offerpageproductname;
+		
+	public TextContextSetup() {
+	
+	
+}
+}
